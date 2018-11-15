a60bcdda0e7aefc57ef63a12e82bb315e32395e7
Inkan <inkandev@gmail.com>
2018-11-15 08:20:16 +0100

    Ajout de la classe Joueur

---
+++ Morpion_métier/Joueur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morpion_métier
+{
+    class Joueur
+    {
+        private String nom;
+
+        public Joueur(String nom)
+        {
+            this.nom = nom;
+        }
+    }
+}
