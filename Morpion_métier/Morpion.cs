6ef66cb5b0934c6677348a0acfee23d2db1c92ac
Inkan <inkandev@gmail.com>
2018-11-15 08:51:21 +0100

    Ajout de la classe Morpion

---
+++ Morpion_métier/Morpion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morpion_métier
+{
+    /* Représente un plateau de jeu */
+    public class Morpion
+    {
+        private List<Joueur> listeJoueurs; // Liste des joueurs 
+        private List<Case> cases; // Liste des cases
+        private Joueur joueurCourant; // Joueur dont c'est le tour
+
+        public Morpion()
+        {
+            this.listeJoueurs = new List<Joueur>();
+            this.cases = new List<Case>();
+            this.joueurCourant = listeJoueurs[0];
+        }
+
+        public void ajouterJoueur(Joueur j)
+        {
+            this.listeJoueurs.Add(j);
+        }
+    }
+}
