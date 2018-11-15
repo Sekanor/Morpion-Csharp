d4b817f8cb40880e6a09b5e368973bc1599c654e
Kévin Kib <sekanor21@gmail.com>
2018-11-15 08:38:11 +0100

    Ajout d'un getter pour la propriété Joueur.

--- Morpion_métier/Case.cs
+++ Morpion_métier/Case.cs
@@ -7,12 +7,21 @@ namespace Morpion_métier
 
         // Correspond au joueur qui a marqué la case.
         private Joueur joueur;
+        public Joueur Joueur
+        {
+            get
+            {
+                return this.joueur;
+            }
+        }
 
+        // Constructeur de la classe Case.
         public Case()
         {
             this.joueur = null;
         }
 
+        // Permet à un joueur de marquer la case.
         public void Marquer(Joueur j)
         {
             this.joueur = j;
