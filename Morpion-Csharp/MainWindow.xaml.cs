6b46fa573818f508e9f1a4db61fc6e5b1f0c9fc5
Ange Pagel <inkandev@gmail.com>
2018-11-20 23:35:53 +0100

    Ajout du marquage d'une case au clic
    Une case peut être marquée en cliquant dessus. Si cette case est déjà marquée, un message d'erreur s'affiche

--- Morpion-Csharp/MainWindow.xaml.cs
+++ Morpion-Csharp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Morpion_métier;
 
 namespace Morpion_Csharp
 {
@@ -20,10 +21,31 @@ namespace Morpion_Csharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Morpion morpion;
+
         public MainWindow()
         {
             InitializeComponent();
+            morpion = new Morpion();
         }
 
+        private void Img_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image img = sender as Image;
+            BitmapImage marque;
+
+            // On vérifie que la case ne soit pas déjà marquée
+            if (img.Source.ToString() == "pack://application:,,,/Morpion-Csharp;component/Images/j0.png")
+            {
+                marque = new BitmapImage(new Uri("Images/j1.png", UriKind.Relative));
+                img.Source = marque;
+            }
+
+            // Si la case est déjà marquée, on affiche un message d'erreur
+            else
+            {
+                MessageBox.Show("Cette case est déjà marquée.", "Impossible de cliquer ici");
+            }
+        }
     }
 }
