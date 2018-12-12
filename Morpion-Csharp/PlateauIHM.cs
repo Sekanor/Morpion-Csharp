46061e1e12da5020deff70db632b4f63e95afdc6
Inkan <inkandev@gmail.com>
2018-12-12 08:16:31 +0100

    Création de la classe PlateauIHM

---
+++ Morpion-Csharp/PlateauIHM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Morpion_métier;
+
+namespace Morpion_Csharp
+{
+    class PlateauIHM
+    {
+        private Plateau plateauMorpion;
+        private List<CaseIHM> casesIHM;
+
+        public PlateauIHM(Plateau p)
+        {
+            this.plateauMorpion = p;
+            foreach (Case c in this.plateauMorpion.Cases)
+            {
+                casesIHM.Add(new CaseIHM(c));
+            }
+        }
+
+    }
+}
