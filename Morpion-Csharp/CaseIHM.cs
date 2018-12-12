3f90de8ac6b92cff60a8a542561209d0b76ade7d
Inkan <inkandev@gmail.com>
2018-12-12 08:14:07 +0100

    Création de la classe CaseIHM

---
+++ Morpion-Csharp/CaseIHM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Morpion_métier;
+
+namespace Morpion_Csharp
+{
+    class CaseIHM
+    {
+        private Case caseMorpion;
+        private BitmapImage img;
+
+        public CaseIHM(Case c)
+        {
+            this.caseMorpion = c;
+            this.img = new BitmapImage(new Uri("Images/j0.png", UriKind.Relative));
+        }
+
+    }
+}
